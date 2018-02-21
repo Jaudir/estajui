8c778f043823b5044a68077432192dd1cd978670
Wadson Barbosa <wadson.ayres@gmail.com>
2018-02-21 03:32:18 -0300

    Recuperação via ajax para o campos "Busca Empresa" concluido.

---
+++ scripts/controllers/aluno/cadastrar-dados-estagio-empresa.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once(dirname(__FILE__) . '/../base-controller.php');
+
+$session = getSession();
+
+if($session->isAluno()){
+    if(isset($_POST["estagio_id"])){
+
+
+
+    }
+}
+
+
--- scripts/models/EstagioModel.php
+++ scripts/models/EstagioModel.php
@@ -50,4 +50,8 @@ class EstagioModel extends MainModel {
 			return false;
 		}
 	}
+	public function cadastrarDadosEstagioeEmpresa(){
+
+
+	}
 }
