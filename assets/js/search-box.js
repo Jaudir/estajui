fdb29703fe5c061f29c06239629123c9eb3695c9
Wadson Barbosa <wadson.ayres@gmail.com>
2018-02-21 03:43:49 -0300

    Recuperação via ajax para o campos "Busca Empresa" concluido.

--- assets/js/search-box.js
+++ assets/js/search-box.js
@@ -25,27 +25,32 @@ $(document)
                         },
                         statusCode: {
                             200: function (data) {
-                                if(data['erro']){
-                                    alert("Empresa Não cadastrada");
-                                }else {
-                                $("[name='nome_fantasia'").val(data['nome_fantasia']);
-                                $("[name='cnpj'").val(data['cnpj']);
-                                $("[name='razao_social'").val(data['razao_social']);
-                                $("[name='telefone'").val(data['telefone']);
-                                $("[name='fax'").val(data['fax']);
-                                $("[name='numero_registro'").val(data['numero_registro']);
-                                $("[name='conselho_fiscal'").val(data['conselho_fiscal']);
-                                $("[name='numero'").val(data['numero']);
-                                $("[name='logradouro'").val(data['logradouro']);
-                                $("[name='bairro'").val(data['bairro']);
-                                $("[name='sala'").val(data['sala']);
-                                $("[name='cep'").val(data['cep']);
-                                $("[name='cidade'").val(data['cidade']);
-                                $("[name='nome_responsavel'").val(data['nome_responsavel']);
-                                $("[name='telefone_responsavel'").val(data['telefone_responsavel']);
-                                $("[name='email'").val(data['email']);
-                                $("[name='cargo_ocupado'").val(data['cargo_ocupado']);
-                                $("[name='uf'").val(data['uf']);
+                                if (data['erro']) {
+                                    alert("Empresa não cadastrada");
+                                    $('<input>').attr({
+                                        type: 'hidden',
+                                        name: 'novaEmpresa',
+                                        value: '1'
+                                    }).appendTo('form');
+                                } else {
+                                    $("[name='nome_fantasia'").val(data['nome_fantasia']);
+                                    $("[name='cnpj'").val(data['cnpj']);
+                                    $("[name='razao_social'").val(data['razao_social']);
+                                    $("[name='telefone'").val(data['telefone']);
+                                    $("[name='fax'").val(data['fax']);
+                                    $("[name='numero_registro'").val(data['numero_registro']);
+                                    $("[name='conselho_fiscal'").val(data['conselho_fiscal']);
+                                    $("[name='numero'").val(data['numero']);
+                                    $("[name='logradouro'").val(data['logradouro']);
+                                    $("[name='bairro'").val(data['bairro']);
+                                    $("[name='sala'").val(data['sala']);
+                                    $("[name='cep'").val(data['cep']);
+                                    $("[name='cidade'").val(data['cidade']);
+                                    $("[name='nome_responsavel'").val(data['nome_responsavel']);
+                                    $("[name='telefone_responsavel'").val(data['telefone_responsavel']);
+                                    $("[name='email'").val(data['email']);
+                                    $("[name='cargo_ocupado'").val(data['cargo_ocupado']);
+                                    $("[name='uf'").val(data['uf']);
                                 }
                             }
                         }
