a0ab1f0125f73bbfa15b881365dbd74a979ab11f
18p1 <paulomarcio12@hotmail.com>
2018-03-04 09:43:15 -0300

    removendo a busca da empresa que foi implementado por alguém

--- estajui/estudante/cadastrar-dados-estagio.php
+++ estajui/estudante/cadastrar-dados-estagio.php
@@ -27,7 +27,7 @@
   function readonly(){
     global $session;
 
-    if($session->hasValues('busca'))
+    if($session->hasValues('busca') && !$session->hasError())
       echo ' readonly';
   }
 
@@ -293,7 +293,7 @@
     <script src="../../assets/js/jquery-3.3.1.min.js" type="text/javascript" charset="utf-8"></script>
     <script src="../../assets/js/jquery.maskedinput.js" type="text/javascript"></script>
     <script src="../../assets/js/masks.js" type="text/javascript"></script>
-    <script src="../../assets/js/search-box.js" type="text/javascript"></script>
+    <!--<script src="../../assets/js/search-box.js" type="text/javascript"></script>-->
     <!--<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>-->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
